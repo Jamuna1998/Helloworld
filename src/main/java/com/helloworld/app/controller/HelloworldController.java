225d038dbee641e0c06074ac70cd5bfb215a5a74
Jamuna1998 <jamunasurya1998@gmail.com>
2022-01-04 11:51:40 +0530

    Helloworld spring boot

---
+++ src/main/java/com/helloworld/app/controller/HelloworldController.java
@@ -0,0 +1,12 @@
+package com.helloworld.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloworldController {
+	@RequestMapping("/")
+	  String helloWorld() {
+	    return "Hello World!";
+	  }
+}
